520f02e09e77fc6e41945e3ff0f0b7bcfa1dc115
JairoC21 <136031928+JairoC21@users.noreply.github.com>
2023-06-21 23:30:03 +0000

    modulos

---
+++ Programa grupal.py
@@ -0,0 +1,5 @@
+from matematica import sumar, restar, multiplicar, dividir
+
+print(f"{temperature_F} grados Fahrenheit equivalen a {temperature_K} grados Kelvin.")
+print(f"{temperature_C} grados Celsius equivalen a {temperature_R} grados Rankine.")
+print(f"{temperature_C} grados Celsius equivalen a {temperature_F} grados Fahrenheit.")
