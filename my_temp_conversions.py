6f70b04e1e7e518f34e209a0266d55f6276433b7
JairoC21 <136031928+JairoC21@users.noreply.github.com>
2023-06-23 01:56:43 +0000

    .......

--- my_temp_conversions.py
+++ my_temp_conversions.py
@@ -1,11 +1,11 @@
-def F_to_K(F):
-   return  (5/9) * (temperature_F - 32) + 273.15
+def F_to_K(F ):
+   return  (5/9) * (F - 32) + 273.15
 
 
-def C_to_R(C):
-    return temperature_C * 1.8 + 32 + 459.67
+def C_to_R(C ):
+    return C * 1.8 + 32 + 459.67
     
 
-def C_to_F(C):
-   return temperature_C * 1.8 + 32
+def C_to_F(C ):
+   return C * 1.8 + 32
     
